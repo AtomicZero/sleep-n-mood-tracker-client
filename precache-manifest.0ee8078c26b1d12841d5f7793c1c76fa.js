b9170eb9ea5e7482048a48603c28d328a4a8797e
AtomicZero <atomiczero@gmail.com>
2020-09-25 22:38:27 +0100

    Updates

---
+++ precache-manifest.0ee8078c26b1d12841d5f7793c1c76fa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d38a73ff5dfbf6cfd109e555774537",
+    "url": "/sleep-n-mood-tracker-client/index.html"
+  },
+  {
+    "revision": "31835e8c7d050c46f859",
+    "url": "/sleep-n-mood-tracker-client/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "b08b93dae050d4f702f9",
+    "url": "/sleep-n-mood-tracker-client/static/css/main.3d6103cd.chunk.css"
+  },
+  {
+    "revision": "31835e8c7d050c46f859",
+    "url": "/sleep-n-mood-tracker-client/static/js/2.0df8c93a.chunk.js"
+  },
+  {
+    "revision": "a094b9fcc0839e14d1c05d999e7f0350",
+    "url": "/sleep-n-mood-tracker-client/static/js/2.0df8c93a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b08b93dae050d4f702f9",
+    "url": "/sleep-n-mood-tracker-client/static/js/main.924054fc.chunk.js"
+  },
+  {
+    "revision": "5e51b63ada156c2a1f9a",
+    "url": "/sleep-n-mood-tracker-client/static/js/runtime-main.dc10028d.js"
+  },
+  {
+    "revision": "3f5353e5d1edda3c86ee377c5c38a890",
+    "url": "/sleep-n-mood-tracker-client/static/media/4k-night-forest-material-design-art.3f5353e5.jpg"
+  }
+]);
